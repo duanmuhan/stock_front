fd877b80343342eec9258cafb5dd2af24fe26439
caoguangshu <caoguangshu@corp.netease.com>
2020-06-30 23:30:56 +0800

    提交ajax代码

--- public/js/shared/net.js
+++ public/js/shared/net.js
@@ -1,18 +1,24 @@
 $(function () {
+    var dataset;
     $("#stock-input").bind('keyup',function () {
-        getStockInfo();
+        var value = $("#stock-input").val();
+        if (!isNaN(value)){
+            getStockInfo("stockId",value);
+        }else {
+            getStockInfo("stockName",value);
+        }
     })
     
-    function getStockInfo() {
+    function getStockInfo(paramName,paramValue) {
         $.ajax({
             type:"GET",
-            url:"127.0.0.1:8080/stockList",
+            url:"http://127.0.0.1:8080/stockList?" + paramName + "=" + paramValue,
             async: false,
             beforeSend:function(){
                 console.log("start to request /stockList")
             },
             success:function (result) {
-                console.log(result)
+                fillData(result.data)
             },
             error:function(e){
                 console.log("function error")
@@ -21,4 +27,12 @@ $(function () {
             }
         });
     }
+
+    function fillData(data) {
+        var displayData = data.map(x => {return x.stockId + "(" + x.stockName + ")"})
+        $('#stock-input').typeahead({
+
+        })
+    }
+
 });
