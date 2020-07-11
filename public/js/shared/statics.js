36255d38eb9ff6ba1e5cddfce003369c22913880
caoguangshu <caoguangshu@corp.netease.com>
2020-07-12 02:16:09 +0800

    提交对应的代码

--- public/js/shared/statics.js
+++ public/js/shared/statics.js
@@ -37,6 +37,8 @@ function drawScatterDiagram(data) {
     var perCommonMinShare = d3.min(data.map(e=>e.basicEarningsPerCommonShare));
     var x_scale = d3.scaleLog().domain([minPrice, maxPrice]).range([0,width]);
     var y_scale = d3.scalePow().exponent(0.3).domain([perCommonMinShare, perCommonMaxShare]).range([0,height]);
+    var tooltip = d3.select("circle").append("div")
+        .attr("class","tooltip")
     svg.selectAll("circle").data(data).enter()
         .append("circle")
         .attr("cx", function(d) {
@@ -45,11 +47,43 @@ function drawScatterDiagram(data) {
         .attr("cy", function(d) {
             return height - y_scale(d.basicEarningsPerCommonShare);
         })
-        .attr("r", 2)
+        .attr("r", 4)
         .attr("fill","red")
-        .on("mousehover",function (d) {
-            
+        .on("mouseenter",function (d) {
+            var x = x_scale(d.price);
+            var y = height - y_scale(d.basicEarningsPerCommonShare);
+            console.log("mouseover");
+            svg.append("text")
+                .attr("id", "tooltip")
+                .attr("x", x)
+                .attr("y", y)
+                .attr("text-anchor", "middle")
+                .attr("font-family", "sans-setif")
+                .attr("font-size", "11px")
+                .attr("font-weight", "bold")
+                .attr("fill", "blue")
+                //文本内容
+                .text("股票id" + d.stockId);
         })
+        .on("mouseout",function (d) {
+            d3.select("#tooltip").remove();
+        }).on("mouseover",function(d) {
+            var x = x_scale(d.price);
+            var y = height - y_scale(d.basicEarningsPerCommonShare);
+            console.log("mouseover");
+            svg.append("text")
+                .attr("id", "tooltip")
+                .attr("x", x)
+                .attr("y", y)
+                .attr("text-anchor", "middle")
+                .attr("font-family", "sans-setif")
+                .attr("font-size", "11px")
+                .attr("font-weight", "bold")
+                .attr("fill", "blue")
+                //文本内容
+                .text("股票id" + d.stockId);
+        }
+    )
     var scale_x = d3.scaleLog().domain([minPrice,maxPrice]).range([20,w]);
     var scale_y = d3.scalePow().domain([perCommonMinShare,perCommonMaxShare]).range([h-20,0]);
     svg.append("g").attr("font-size","20").attr('transform', 'translate(0,' + height + ')').call(d3.axisBottom(scale_x));
@@ -58,8 +92,4 @@ function drawScatterDiagram(data) {
 
 function getColor(kItem) {
     return kItem.openPrice <= kItem.closePrice ? "red":"green";
-}
-
-function showTip() {
-
 }
