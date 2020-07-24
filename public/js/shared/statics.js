b7dd9b4b39064392f15754a41a36e7405c10f517
caoguangshu <caoguangshu@corp.netease.com>
2020-07-24 20:44:54 +0800

    提交代码

--- public/js/shared/statics.js
+++ public/js/shared/statics.js
@@ -3,6 +3,7 @@ $(function () {
     fetchStockMarketValue();
     fetchStockChangeValue();
     fetchStockPriceHistValue();
+    fetchStockHolderConcentrate();
 })
 
 function fetchPriceAndShare() {
@@ -81,6 +82,27 @@ function fetchStockPriceHistValue() {
             console.log(result.data)
             d3.select("#stockPriceHistSvg").remove()
             drawStockPriceHist(result.data.list);
+        },
+        error:function(e){
+            console.log("function error")
+            console.log(e.status)
+            console.log(e.responseText)
+        }
+    })
+}
+
+function fetchStockHolderConcentrate() {
+    $.ajax({
+        type:"GET",
+        url:"http://127.0.0.1:8080/stockholder/rate/hist",
+        async: false,
+        beforeSend:function(){
+            console.log("start to request /stockholder/rate/hist")
+        },
+        success:function (result) {
+            console.log(result.data)
+            d3.select("#stockHolderConcentrateHist").remove()
+            drawStockHolderConcentrate(result.data.list);
         },
         error:function(e){
             console.log("function error")
@@ -286,6 +308,52 @@ function drawStockPriceHist(data) {
 
     svg.append("g")
         .call(yAxis).attr("transform", `translate(${margin.left},0)`);
+}
+
+function drawStockHolderConcentrate(data) {
+    var margin = ({top: 30, right: 0, bottom: 30, left: 40})
+    var h = $("#stockHolderHist").height();
+    var w = $("#stockHolderHist").width();
+    var svg = d3.select("#stockHolderHist").append('svg').attr('width', w).attr('height', h).attr("id","stockHolderConcentrateHist").style("background", "#ffffff");
+    var xScale = d3.scaleBand()
+        .domain(d3.range(data.length))
+        .range([margin.left, w - margin.right])
+        .padding(0.1);
+
+    var yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.value)]).nice()
+        .range([h - margin.bottom, margin.top]);
+
+    var xAxis = d3.axisBottom(xScale).tickFormat(i => data[i].key).tickSizeOuter(0);
+    var yAxis = d3.axisLeft(yScale).ticks(null, data.format);
+
+    svg.append("g")
+        .attr("fill", "LightGreen")
+        .selectAll("rect")
+        .data(data)
+        .join("rect")
+        .attr("x", (d, i) => xScale(i))
+        .attr("y", d => yScale(d.value))
+        .attr("height", d => yScale(0) - yScale(d.value))
+        .attr("width", xScale.bandwidth());
+    svg.append("g").selectAll("text")
+        .data(data)
+        .attr("font-size", 12)
+        .join("text")
+        .attr("text-anchor", "begin")
+        .attr("x", (d,i) => xScale(i) + xScale.bandwidth() / 3)
+        .attr("y", d => yScale(d.value))
+        .text(d => d.value);
+
+    svg.append("g")
+        .call(xAxis).attr("transform", `translate(0,${h - margin.bottom})`);
+
+    svg.append("g")
+        .call(yAxis).attr("transform", `translate(${margin.left},0)`);
+}
+
+function bindDraggableDiv() {
+
 }
 
 function arcLabel() {
