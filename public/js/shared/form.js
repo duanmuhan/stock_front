0198f53b6f78d2930d70859651093cf0136038b6
caoguangshu <caoguangshu@corp.netease.com>
2020-08-12 23:55:49 +0800

    提交代码

--- public/js/shared/form.js
+++ public/js/shared/form.js
@@ -1,88 +1,173 @@
 $(function(){
-
-     render(currentStartPagePage);
-     renderHighestIncreaseShare(currentStartPagePage);
-     renderHighestPeriodIncreaseShare(currentStartPagePage);
+$.extend($.fn.bootstrapTable.Defaults, $.fn.bootstrapTable.locales['zh-CN'])
+     renderTopValuePerPrice();
+     renderHighestIncreaseShare();
+     renderHighestPeriodIncreaseShare();
      renderStockAchievement(currentStartPagePage);
      renderStockScore(currentStartPagePage);
      renderStockTechnology(currentStartPagePage)
 })
 var pageSize = 10;
 var currentStartPagePage = 1;
-function render(currentPage){
-            $.ajax({
-                type:"GET",
-                url:"http://127.0.0.1:8080/topValuePerPrice/form",
-                data:{
-                    pageNo : currentPage,
-                    pageSize : pageSize
-                },
-                async: false,
-                beforeSend:function(){
-                    console.log("start to request /topValuePerPrice/form")
-                },
-                success:function (result) {
-                    if (result.code != 200){
-                        alert("error:" + result.message);
-                    }
-                    latestShareBonus(result.data.list);
-                    $("#pagination").html(buildPagination(currentPage,200,render));
-                },
-                error:function(e){
-                    console.log("function error:{}",e)
-                }
-            });
+function renderTopValuePerPrice(){
+                $('#latestShareBonus').bootstrapTable({
+                    url: "http://127.0.0.1:8080/topValuePerPrice/form",
+                    toolbar: '#toolbar',
+                    striped: true,                      //是否显示行间隔色
+                    cache: false,                       //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+                    pagination : true,                   //是否显示分页（*）
+                    sortable: false,                     //是否启用排序                 //排序方式
+                    queryParams: function(params){
+                        console.log(params);
+                        return {
+                            pageSize: params.limit,
+                            pageNo: params.offset/params.limit
+                        }
+                    },         //传递参数（*）
+                    sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
+                    pageNumber:1,                       //初始化加载第一页，默认第一页
+                    pageSize: 10,                       //每页的记录行数（*）
+                    pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
+                    smartDisplay:false,
+                    undefinedText: '---',
+                    showColumns: false,                 //是否显示所有的列
+                    showRefresh: false,                 //是否显示刷新按钮
+                    minimumCountColumns: 1,             //最少允许的列数
+                    clickToSelect: true,                //是否启用点击选中行
+                    showToggle:false,                   //是否显示详细视图和列表视图的切换按钮
+                    cardView: false,                    //是否显示详细视图
+                    detailView: false,                 //是否显示父子表
+                    onLoadSuccess: function(data){
+                       $('#latestShareBonus').bootstrapTable('load', data.data);
+                    },
+                    columns: [
+                        {
+                            field:'stockId',
+                            title:'股票id',
+                        },{
+                            field:'stockName',
+                            title:'股票名称',
+                        },{
+                            field:'price',
+                            title:'股票价格',
+                        },{
+                            field:'earningsPerPrice',
+                            title:'单位价格分红',
+                        },{
+                            field:'date',
+                            title:'更新日期',
+                        }
+                    ],
+                });
 
 }
 
-function renderHighestIncreaseShare(currentPage){
-                $.ajax({
-                    type:"GET",
-                    url:"http://127.0.0.1:8080/stock/increase/rank",
-                    data:{
-                        pageNo : currentPage,
-                        pageSize : pageSize
-                    },
-                    async: false,
-                    beforeSend:function(){
-                        console.log("start to request /stock/increase/rank")
-                    },
-                    success:function (result) {
-                        if (result.code != 200){
-                            alert("error:" + result.message);
-                        }
-                        highestIncreaseShare(result.data);
-                        $("#stockIncrease-pagination").html(buildPagination(currentPage,200,renderHighestIncreaseShare));
-                    },
-                    error:function(e){
-                        console.log("function error:{}",e)
-                    }
-                });
+function renderHighestIncreaseShare(){
+    var dataList;
+    $('#highestIncreaseTable').bootstrapTable({
+        url: "http://127.0.0.1:8080/stock/increase/rank",
+        toolbar: '#toolbar',
+        striped: true,                      //是否显示行间隔色
+        cache: false,                       //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+        pagination : true,                   //是否显示分页（*）
+        sortable: false,                     //是否启用排序                 //排序方式
+        queryParams: function(params){
+            console.log(params);
+            return {
+                pageSize: params.limit,
+                pageNo: params.offset/params.limit
+            }
+        },         //传递参数（*）
+        sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
+        pageNumber:1,                       //初始化加载第一页，默认第一页
+        pageSize: 10,                       //每页的记录行数（*）
+        pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
+        smartDisplay:false,
+        undefinedText: '---',
+        showColumns: false,                 //是否显示所有的列
+        showRefresh: false,                 //是否显示刷新按钮
+        minimumCountColumns: 1,             //最少允许的列数
+        clickToSelect: true,                //是否启用点击选中行
+        showToggle:false,                   //是否显示详细视图和列表视图的切换按钮
+        cardView: false,                    //是否显示详细视图
+        detailView: false,                 //是否显示父子表
+        onLoadSuccess: function(data){
+           $('#highestIncreaseTable').bootstrapTable('load', data.data);
+        },
+        columns: [
+            {
+                field:'stockId',
+                title:'股票id',
+            },{
+                field:'stockName',
+                title:'股票名称',
+            },{
+                field:'price',
+                title:'股票价格',
+            },{
+                field:'changeRate',
+                title:'股票涨幅',
+            },{
+                field:'date',
+                title:'更新日期',
+            }
+        ],
+    });
 }
 
-function renderHighestPeriodIncreaseShare(currentPage){
-                $.ajax({
-                    type:"GET",
-                    url:"http://127.0.0.1:8080/stock/rate/rank",
-                    data:{
-                        pageNo : currentPage,
-                        pageSize : pageSize
-                    },
-                    async: false,
-                    beforeSend:function(){
-                        console.log("start to request /stock/rate/rank")
-                    },
-                    success:function (result) {
-                        if (result.code != 200){
-                            alert("error:" + result.message);
-                        }
-                        highestPeriodIncreaseShare(result.data);
-                        $("#stock-period-Increase-pagination").html(buildPagination(currentPage,200,renderHighestPeriodIncreaseShare));
-                    },
-                    error:function(e){
-                        console.log("function error:{}",e)
-                    }
-                });
+function renderHighestPeriodIncreaseShare(){
+    $('#highestPeriodIncreaseShare').bootstrapTable({
+        url: "http://127.0.0.1:8080/stock/rate/rank",
+        toolbar: '#toolbar',
+        striped: true,                      //是否显示行间隔色
+        cache: false,                       //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+        pagination : true,                   //是否显示分页（*）
+        sortable: false,                     //是否启用排序                 //排序方式
+        queryParams: function(params){
+            console.log(params);
+            return {
+                pageSize: params.limit,
+                pageNo: params.offset/params.limit
+            }
+        },         //传递参数（*）
+        sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
+        pageNumber:1,                       //初始化加载第一页，默认第一页
+        pageSize: 10,                       //每页的记录行数（*）
+        pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
+        smartDisplay:false,
+        undefinedText: '---',
+        showColumns: false,                 //是否显示所有的列
+        showRefresh: false,                 //是否显示刷新按钮
+        minimumCountColumns: 1,             //最少允许的列数
+        clickToSelect: true,                //是否启用点击选中行
+        showToggle:false,                   //是否显示详细视图和列表视图的切换按钮
+        cardView: false,                    //是否显示详细视图
+        detailView: false,                 //是否显示父子表
+        onLoadSuccess: function(data){
+           $('#highestPeriodIncreaseShare').bootstrapTable('load', data.data);
+        },
+        columns: [
+            {
+                field:'stockId',
+                title:'股票id',
+            },{
+                field:'stockName',
+                title:'股票名称',
+            },{
+                field:'price',
+                title:'当前股票价格',
+            },{
+                field:'changeRate',
+                title:'股票涨幅',
+            },{
+                field:'fromDate',
+                title:'起始日期',
+            },{
+                field:'toDate',
+                title:'截止日期',
+             }
+        ],
+    });
 }
 
 function renderStockAchievement(currentPage){
@@ -102,7 +187,7 @@ function renderStockAchievement(currentPage){
                                 alert("error:" + result.message);
                             }
                             stockAchievement(result.data);
-                            $("#ststock-achievement-Increase-pagination").html(buildPagination(currentPage,200,renderStockAchievement));
+                            $("#stock-achievement-Increase-pagination").html(buildPagination(currentPage,200,renderStockAchievement));
                         },
                         error:function(e){
                             console.log("function error:{}",e)
@@ -115,8 +200,8 @@ function renderStockScore(currentPage){
                     type:"GET",
                     url:"http://127.0.0.1:8080/stock/technology/score",
                     data:{
-                        pageNo : currentPage,
-                        pageSize : pageSize
+                         pageNo : currentPage,
+                         pageSize : pageSize
                     },
                     async: false,
                     beforeSend:function(){
