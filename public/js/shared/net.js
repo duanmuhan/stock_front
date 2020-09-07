e105b540eefd9764d08bdf9adbd1e4e3898db25a
caoguangshu <caoguangshu@corp.netease.com>
2020-09-08 00:26:18 +0800

    提交均线代码

--- public/js/shared/net.js
+++ public/js/shared/net.js
@@ -207,10 +207,6 @@ function drawAverage(averageDataSet,kDataSet) {
     var scale_y = d3.scaleLinear().domain([0.8*minPrice, 1.2*maxPrice]).nice().range([height - margin.bottom, margin.top]);
     var svg = d3.select("#stock-index").append('svg').attr('width', width).attr('height', height).attr("id","stock-index-content").style("background", "#ffffff");
     var line = d3.line().x(d=>scale_x(convertDateFromString(d.date))).y(d=>scale_y(d.price));
-    var tenDayAverageLine = d3.line().x(d=>scale_x(convertDateFromString(d.date))).y(d=>scale_y(d.price));
-    var twentiesDayAverageLine = d3.line().x(d=>scale_x(convertDateFromString(d.date))).y(d=>scale_y(d.price));
-    var sixtiesDayAverageLine = d3.line().x(d=>scale_x(convertDateFromString(d.date))).y(d=>scale_y(d.price));
-    var oneHundredAndSixtiesDayAverageLine = d3.line().x(d=>scale_x(convertDateFromString(d.date))).y(d=>scale_y(d.price));
     svg.append("path")
         .datum(averageDataSet.fiveDayList)
         .attr("fill", "none")
@@ -218,34 +214,6 @@ function drawAverage(averageDataSet,kDataSet) {
         .attr("stroke-width", 1.5)
         .attr("stroke-linejoin", "round")
         .attr("stroke-linecap", "round").attr("d", line);
-    svg.append("path")
-        .datum(averageDataSet.tenDayList)
-        .attr("fill", "none")
-        .attr("stroke", "red")
-        .attr("stroke-width", 1.5)
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round").attr("d", tenDayAverageLine);
-    svg.append("path")
-        .datum(averageDataSet.twentiesDayList)
-        .attr("fill", "none")
-        .attr("stroke", "green")
-        .attr("stroke-width", 1.5)
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round").attr("d", twentiesDayAverageLine);
-    svg.append("path")
-        .datum(averageDataSet.sixtiesDayList)
-        .attr("fill", "none")
-        .attr("stroke", "black")
-        .attr("stroke-width", 1.5)
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round").attr("d", sixtiesDayAverageLine);
-    svg.append("path")
-        .datum(averageDataSet.oneHundredAndTwentiesDayList)
-        .attr("fill", "none")
-        .attr("stroke", "yellow")
-        .attr("stroke-width", 1.5)
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round").attr("d", oneHundredAndSixtiesDayAverageLine);
     svg.append("g").attr("font-size","20").attr("transform", `translate(0,${height - margin.bottom})`).call(d3.axisBottom(scale_x).ticks(width / 80).tickSizeOuter(0));
     svg.append("g").attr("font-size","20").attr("transform", `translate(${margin.left},0)`).call(d3.axisLeft().scale(scale_y)).call(g => g.select(".domain").remove())
         .call(g => g.select(".tick:last-of-type text").clone()
